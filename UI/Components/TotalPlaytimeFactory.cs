42ff02c4fdad65bcd32a54e83d77bf951558adf4
Sergey Papushin <spapushin@gmail.com>
2015-08-29 14:47:55 -0500

    Update Component Version to 1.6

--- UI/Components/TotalPlaytimeFactory.cs
+++ UI/Components/TotalPlaytimeFactory.cs
@@ -46,7 +46,7 @@ namespace LiveSplit.UI.Components
 
         public Version Version
         {
-            get { return Version.Parse("1.0.0"); }
+            get { return Version.Parse("1.6"); }
         }
 
         public string UpdateName
