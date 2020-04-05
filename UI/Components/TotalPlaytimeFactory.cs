8443b44851da686e14fe4a48e065907578cfe319
wooferzfg <spapushin@gmail.com>
2020-04-05 14:34:21 -0500

    Update version to 1.8.0

--- UI/Components/TotalPlaytimeFactory.cs
+++ UI/Components/TotalPlaytimeFactory.cs
@@ -20,7 +20,7 @@ namespace LiveSplit.UI.Components
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.7.7");
+        public Version Version => Version.Parse("1.8.0");
 
         public string UpdateName => ComponentName;
     }
